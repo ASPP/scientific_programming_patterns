3fda6ca84ff6f083dea463782e4c0fb68abec922
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2026-08-31 15:06:16 +0200

    version 120000345 of the conceptual exercises

--- class_materials/walker/Step_5_reproducibility/run.py
+++ class_materials/walker/Step_5_reproducibility/run.py
@@ -8,28 +8,30 @@ import context_maps
 from walker import Walker
 
 # Use the following parameters to simulate and save a trajectory of the walker
-
-seed = 42
 sigma_i = 3
 sigma_j = 4
 size = 200
 i, j = (50, 100)
 n_iterations = 1000
-# USE map_type hills
-random_state = np.random.RandomState(seed)
+random_state = np.random.RandomState()
 
-# STEP 1: Create a context map
+# RUN YOUR SIMULATION
+# Create a context map (take whichever one you like best!)
+context_map = context_maps.hills_context_map_builder(size)
 
+# Create a Walker
+walker = Walker(sigma_i, sigma_j, context_map)
 
-# STEP 2: Create a Walker
-
-
-# STEP 3: Simulate the walk
-
+# Simulate the walk
+trajectory = []
+for _ in range(n_iterations):
+    i, j = walker.sample_next_step(i, j, random_state)
+    trajectory.append((i, j))
 
 # STEP 4: Save the trajectory
 curr_time = time.strftime("%Y%m%d-%H%M%S")
 # save the npy file here!
+# ...
 
 # STEP 5: Save the metadata
 # lookup git repository
@@ -37,6 +39,6 @@ repo = git.Repo(search_parent_directories=True)
 sha = repo.head.object.hexsha
 
 with open('meta.txt', 'w') as f:
-    f.write(f'I estimated parameters at {curr_time}.\n')
-    f.write(f'The git repo was at commit {sha}')
-    # you can add any other information you want here!
+    # you can add any information you want here!
+    # e.g. f.write(f'The git repo was at commit {sha}')
+    # ...
